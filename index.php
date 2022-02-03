d49ad53ae9a4235c63a85560d6af3ced5c47640a
malithdayanga <malithdayanga00@gmail.com>
2022-02-03 13:50:29 +0530

    I create this aaplication for testing purpose

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="plugins/bootstrap.min.css">
+    <script src="plugins/jquery.min.js"></script>
+    <script src="plugins/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php include_once("inc/navbar.php");?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron mt-4">
+                    <h4 id="jumbo-header">Welcome to MD coders</h4>
+                    <h4 id="jumbo-emoji">👾👨‍💻👾</h4>
+                </div>   
+            </div>
+        </div>
+
+    </div>
+ 
+</body>
+</html>
